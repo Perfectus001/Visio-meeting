cb43452984db197fc8a7abafafea9707887f22a6
Perfectus001 <jeanmikely001@gmail.com>
2025-08-09 19:16:26 -0400

    After modify and add migration messages, appointments

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('firstname');
+            $table->enum('sexe', ['M', 'F'])->default('M'); // M, F
+            $table->date('birthdate')->nullable(false);
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->enum('role', ['admin', 'user'])->default('user'); // admin, user
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
